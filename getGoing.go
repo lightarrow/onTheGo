1c6bad7352410331dde2ee189f59cac7ec615ee8
Ronald Johnson <lightarrow@icloud.com>
2022-01-07 18:50:06 -0600

    Getting used to Go

---
+++ getGoing.go
@@ -0,0 +1,20 @@
+// package onTheGo
+/*
+ * Author Ronald Johnson 2022
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome Gophers")
+	var x int
+	var y int
+	x = 1
+	y = 2
+	fmt.Printf("x=%v, type of %T\n", x, x)
+	fmt.Printf("x=%v, type of %T\n", y, y)
+
+}
